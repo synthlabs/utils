428ce12aad0314e469dcc6d66dd046579b0068a3
Jerod <jerod@synth.sh>
2025-12-06 14:50:53 -0800

    misc(updater): link to latest release

--- js/updater.ts
+++ js/updater.ts
@@ -3,14 +3,14 @@ import { check } from '@tauri-apps/plugin-updater';
 import { ask } from '@tauri-apps/plugin-dialog';
 import { relaunch } from '@tauri-apps/plugin-process';
 
-export async function checkForAppUpdates() {
+export async function checkForAppUpdates(release_url: string) {
 	const update = await check();
 	if (!update) {
 		console.log('No update available');
 	} else if (update) {
-		console.log('Update available!', update.version, update.body);
+		console.log('Update available!', update);
 		const yes = await ask(
-			`Update to ${update.version} is available!\n\nRelease notes: ${update.body}`,
+			`Update to ${update.version} is available!\n\nRelease notes: ${release_url}`,
 			{
 				title: 'Update Available',
 				kind: 'info',
