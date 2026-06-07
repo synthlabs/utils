4a7ea69dbdc110d7ef0ce6264851da358f3bf396
Jerod <jerod@synth.sh>
2026-06-07 08:52:27 -0700

    fix(ui): updater.ts update for sonner api change

--- js/updater.ts
+++ js/updater.ts
@@ -143,6 +143,9 @@ export async function checkForAppUpdates(
         },
         cancel: {
             label: copy.dismiss,
+            onClick: (event) => {
+                event.preventDefault();
+            },
         },
     });
 }
